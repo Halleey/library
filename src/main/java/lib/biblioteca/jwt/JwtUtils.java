1525ff05e0ee4f97d5c89fb20443860a63b5d6a5
Hallyson <aparecidohallyson@gmail.com>
2024-04-12 11:03:15 -0300

    token inicializado

--- src/main/java/lib/biblioteca/jwt/JwtUtils.java
+++ src/main/java/lib/biblioteca/jwt/JwtUtils.java
@@ -1,9 +1,8 @@
 package lib.biblioteca.jwt;
 
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -11,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
+@Slf4j
 public class JwtUtils {
     public static final String JWT_BEARER = "Bearer ";
     public static final String JWT_AUTHORIZATION = "Authorization";
@@ -19,39 +19,68 @@ public class JwtUtils {
     public static final long EXPIRE_HOURS = 0;
     public static final long EXPIRE_MINUTES = 2;
 
-    public JwtUtils(){
-
+    private JwtUtils(){
     }
 
-    private static Key generatedKeys() {
+    private static Key generateKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
-    private static Date toExpireData(Date start) {
-        // Converte o Date inicial para LocalDateTime para facilitar a manipulação
+
+    private static Date toExpireDate(Date start) {
         LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-
-        // Adiciona dias, horas e minutos para definir a data de expiração
-        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS)
-                .plusHours(EXPIRE_HOURS)
-                .plusMinutes(EXPIRE_MINUTES);
-
-        // Converte a LocalDateTime final de volta para um objeto Date
-        // para manter a compatibilidade com sistemas mais antigos que usam Date
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
         return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
     }
 
     @Deprecated
-    public static JwtToken createToken (String username, String role) {
-        Date issueAt = new Date();
-        Date limit  = toExpireData(issueAt);
+    public static JwtToken createToken(String username, String role) {
+        Date issuedAt = new Date();
+        Date limit = toExpireDate(issuedAt);
 
-        String token = Jwts.builder().setHeaderParam("typ", "JWT")
-                .setSubject(username).
-                setIssuedAt(issueAt).
-                setExpiration(limit).
-                signWith(generatedKeys(), SignatureAlgorithm.ES256)
-                .claim("role", role).compact();
-           return new JwtToken(token);
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(username)
+                .setIssuedAt(issuedAt)
+                .setExpiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role", role)
+                .compact();
+
+        return new JwtToken(token);
     }
 
+    @Deprecated
+    private static Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refactorToken(token)).getBody();
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalido %s", ex.getMessage()));
+        }
+        return null;
+    }
+
+    public static String getUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public static boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refactorToken(token));
+            return true;
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalido %s", ex.getMessage()));
+        }
+        return false;
+    }
+
+    private static String refactorToken(String token) {
+        if (token.contains(JWT_BEARER)) {
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
+    }
 }
